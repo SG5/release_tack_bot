3fbe0ccb8150a29961c139b527e3e56740404389
SG5 <sergeyv89@gmail.com>
2017-06-16 12:59:26 +0300

    add flask for heroku

--- app.py
+++ app.py
@@ -1,34 +1,50 @@
+from flask import Flask
 from provider.packagist import *
 from pymongo import MongoClient
 
 import os
 import telegram
 
+app = Flask(__name__)
+
 client = MongoClient(os.environ['MONGODB_CONNECT'])
 db = client.releases
 
 bot = telegram.Bot(token=os.environ['TELEGRAM_BOT_TOKEN'])
 
-for item in db.tasks.find():
-    api = Packagist(item['product'])
-    last_release = None
 
-    if 'version' not in item:
-        releases = api.get_releases()
-        if len(releases):
-            last_release = api.get_releases()[-1]
-    else:
-        last_release = api.get_new_version(item['version'])
+@app.route("/")
+def index():
+    return 'Hello World'
 
-        if last_release is not None:
-            for consumer in db.consumers.find({'task': item['_id']}):
-                bot.send_message(chat_id=consumer['chat_id'], text='Вышла новая версия {}:  {}'.format(
-                    item.get('description', last_release['description']),
-                    last_release['version']
-                ))
 
-    if last_release and item.get('version') != last_release['version']:
-        db.tasks.update_one(
-            {'_id': item['_id']},
-            {'$set': {'version': last_release['version']}}
-        )
+@app.route("/tasks")
+def tasks():
+    for item in db.tasks.find():
+        api = Packagist(item['product'])
+        last_release = None
+
+        if 'version' not in item:
+            releases = api.get_releases()
+            if len(releases):
+                last_release = api.get_releases()[-1]
+        else:
+            last_release = api.get_new_version(item['version'])
+
+            if last_release is not None:
+                for consumer in db.consumers.find({'task': item['_id']}):
+                    bot.send_message(chat_id=consumer['chat_id'], text='Вышла новая версия {}:  {}'.format(
+                        item.get('description', last_release['description']),
+                        last_release['version']
+                    ))
+
+        if last_release and item.get('version') != last_release['version']:
+            db.tasks.update_one(
+                {'_id': item['_id']},
+                {'$set': {'version': last_release['version']}}
+            )
+
+    return 'ok'
+
+if __name__ == "__main__":
+    app.run()
