72a9f0180ab6b390614f64ae6299bb1228106305
SG5 <sergeyv89@gmail.com>
2018-03-15 18:12:14 +0300

    change python version

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 from tasks import mongo_tasks
 
@@ -19,4 +21,4 @@ def tasks():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
