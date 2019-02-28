4c0e89bbbedb3ce218bc6137b604699d15a6c224
SG5 <sergeyv89@gmail.com>
2019-02-28 11:00:29 +0300

    separate env variable by bots

--- init.py
+++ init.py
@@ -4,13 +4,15 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from telepot import Bot
 
 app = Sanic()
+dbClient = AsyncIOMotorClient(os.environ['MONGODB_CONNECT'])
+
 
 @app.listener('before_server_start')
 async def open_connection(app, loop):
-    db.client.io_loop = loop
+    dbClient.io_loop = loop
 
-db = AsyncIOMotorClient(os.environ['MONGODB_CONNECT'])[
+releaseDb = dbClient[
     os.environ.get('MONGODB_DATABASE', 'releases')
 ]
 
-bot = Bot(token=os.environ['TELEGRAM_BOT_TOKEN'])
+releaseBot = Bot(token=os.environ['TELEGRAM_RELEASE_BOT_TOKEN'])
--- tasks/new_software.py
+++ tasks/new_software.py
@@ -1,7 +1,7 @@
 import asyncio
 from telepot.namedtuple import InlineKeyboardButton, InlineKeyboardMarkup
 
-from init import db, bot
+from init import releaseDb, releaseBot
 from provider.npmjs import NpmJS
 from provider.packagist import Packagist
 
@@ -13,7 +13,7 @@ PROVIDERS = {
 
 async def mongo_tasks():
     processing = []
-    for task in await db.tasks.find().to_list(1000):
+    for task in await releaseDb.tasks.find().to_list(1000):
         processing.append(process_task(task))
     return await asyncio.wait(processing)
 
@@ -33,14 +33,14 @@ async def process_task(task):
             last_release = releases[-1]
 
     if last_release and task.get('version') != last_release['version']:
-        db.tasks.update_one(
+        releaseDb.tasks.update_one(
             {'_id': task['_id']},
             {'$set': {'version': last_release['version']}}
         )
 
 
 async def notify_consumers(item, release):
-    for consumer in await db.consumers.find(filter={'task': item['_id']}, projection={'chat_id': True}).to_list(1000):
+    for consumer in await releaseDb.consumers.find(filter={'task': item['_id']}, projection={'chat_id': True}).to_list(1000):
         text = 'Вышла новая версия *{}*:  `{}`'.format(
             item.get('description', release['description']),
             release['version']
@@ -64,7 +64,7 @@ async def notify_consumers(item, release):
                 InlineKeyboardButton(text='Github', url=release_url[:-4])
             )
 
-        bot.sendMessage(
+        releaseBot.sendMessage(
             chat_id=consumer['chat_id'], text=text, parse_mode='Markdown',
             reply_markup=InlineKeyboardMarkup(inline_keyboard=reply_links)
         )
